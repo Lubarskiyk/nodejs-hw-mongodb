453fcf69b50ecbd9101ee87e6f9be940334916ef
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-10-23 21:26:16 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import express from 'express';
+import { startServer } from './server.js';
 
-const PORT = 3000;
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+startServer();
