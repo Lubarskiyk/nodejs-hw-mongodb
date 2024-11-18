abac3c5a250a4b647002fede9c8bc612c2255f3a
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-11-18 10:07:14 +0200

    finish

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,141 +1,77 @@
-import createHttpError from 'http-errors';
 import {
-  createContact,
-  deleteContact,
-  getAllContacts,
-  getContactById,
-  updateContact,
-} from '../services/contacts.js';
-import { parsePaginationParams } from '../utils/parsePaginationParams.js';
-import { parseSortParams } from '../utils/parseSortParams.js';
-import { parseFilterParams } from '../utils/parseFilterParams.js';
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshUsersSession,
+} from '../services/auth.js';
+import { THIRTY_DAYS } from '../constants/index.js';
 
-export const getContactsController = async (req, res) => {
-  const { page, perPage } = parsePaginationParams(req.query);
-  const { sortBy, sortOrder } = parseSortParams(req.query);
-  const filter = parseFilterParams(req.query);
-  const userId = req.user._id;
-  const contacts = await getAllContacts({
-    page,
-    perPage,
-    sortBy,
-    sortOrder,
-    filter,
-    userId,
-  });
-
-  res.json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
-};
-
-export const getContactByIdController = async (req, res, next) => {
-  const { contactId } = req.params;
-  const userId = req.user._id;
-  const contact = await getContactById({ contactId, userId });
-
-  if (!contact) {
-    next(createHttpError(404, 'Contact not found!'));
-    return;
-  }
-
-  res.json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact,
-  });
-};
-
-export const createContactController = async (req, res, next) => {
-  const { name, phoneNumber, email, isFavourite, contactType } = req.body;
-  const userId = req.user._id;
-
-  if (!name || !phoneNumber || !contactType) {
-    return next(
-      createHttpError(400, 'Required fields: name, phoneNumber, contactType!'),
-    );
-  }
-
-  const newContact = await createContact({
-    name,
-    phoneNumber,
-    email,
-    isFavourite,
-    contactType,
-    userId,
-  });
+export const registerUserController = async (req, res) => {
+  const user = await registerUser(req.body);
 
   res.status(201).json({
     status: 201,
-    message: 'Successfully created a contact!',
-    data: newContact,
+    message: 'Successfully registered a user!',
+    data: user,
   });
 };
 
-export const upsertContactController = async (req, res, next) => {
-  const { contactId } = req.params;
-  const userId = req.user._id;
-  const { name, phoneNumber, contactType } = req.body;
+export const loginUserController = async (req, res) => {
+  const session = await loginUser(req.body);
 
-  const result = await updateContact({ contactId, userId }, req.body, {
-    upsert: true,
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
   });
-
-  if (!result) {
-    next(createHttpError(404, 'Contact not found!'));
-    return;
-  }
-
-  if (result.isNew) {
-    if (!name || !phoneNumber || !contactType) {
-      return next(
-        createHttpError(
-          400,
-          'Name, Phone Number, Contact Type and User Id are required when creating a contact!',
-        ),
-      );
-    }
-  }
-
-  const status = result.isNew ? 201 : 200;
-
-  res.status(status).json({
-    status,
-    message: result.isNew
-      ? 'Successfully created a contact!'
-      : 'Successfully updated the contact!',
-    data: result.contact,
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
   });
-};
-
-export const patchContactController = async (req, res, next) => {
-  const { contactId } = req.params;
-  const userId = req.user._id;
-  const result = await updateContact({ contactId, userId }, req.body);
-
-  if (!result) {
-    next(createHttpError(404, 'Contact not found!'));
-    return;
-  }
 
   res.json({
     status: 200,
-    message: `Successfully patched a contact!`,
-    data: result.contact,
+    message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
   });
 };
 
-export const deleteContactController = async (req, res, next) => {
-  const { contactId } = req.params;
-  const userId = req.user._id;
-  const contact = await deleteContact({ contactId, userId });
-
-  if (!contact) {
-    next(createHttpError(404, 'Contact not found!'));
-    return;
+export const logoutUserController = async (req, res) => {
+  if (req.cookies.sessionId) {
+    await logoutUser(req.cookies.sessionId);
   }
 
+  res.clearCookie('sessionId');
+  res.clearCookie('refreshToken');
+
   res.status(204).send();
+};
+
+const setupSession = (res, session) => {
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+};
+
+export const refreshUserSessionController = async (req, res) => {
+  const session = await refreshUsersSession({
+    sessionId: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  setupSession(res, session);
+
+  res.json({
+    status: 200,
+    message: 'Successfully refreshed a session!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { startServer } from './server.js';
 import { initMongoDB } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
 
 const bootstrap = async () => {
   await initMongoDB();
-  startServer();
+  setupServer();
 };
 
 bootstrap();
